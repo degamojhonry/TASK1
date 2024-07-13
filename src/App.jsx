f3a1e95f7bfc524617a8b5a260a537360666f66f
degamojhonry <jhonrydegamo@gmail.com>
2024-07-13 08:36:51 +0530

    message

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <h1>First React App Installation</h1>
+      <p>
+        React has solidified its position as the go-to JavaScript front-end
+        framework in the current tech landscape. <br /> It’s fascinating to see
+        how it’s seamlessly woven itself into the development practices of
+        well-established corporations and budding startups alike. <br />{" "}
+        Originally unveiled by Facebook in 2013, React has since evolved into a
+        cornerstone technology, forming the backbone of numerous applications,
+        including those powering the very platform it originated from. <br /> If
+        you’re new to React, this article will help introduce you to the basics
+        . <br /> Whether you’re curious about the fundamental concepts or eager
+        to dive straight into hands-on learning, our Learn React course provides
+        an immersive and practical experience.
+        <p>1. JHONRY C. DEGAMO</p>
+      </p>
+    </div>
+  );
+}
+
+export default App;
